09d18bea28e15c4c031b487b540df1087f8a28cd
fidel <fidel@yafp.de>
2019-12-08 19:48:19 +0100

    20191208 - Several minor changes

--- app/js/crashReporting.js
+++ app/js/crashReporting.js
@@ -3,13 +3,13 @@
 // Error Handling using: crashReporter (https://electronjs.org/docs/api/crash-reporter)
 // ----------------------------------------------------------------------------
 //
-const { crashReporter } = require("electron");
+const { crashReporter } = require('electron')
 crashReporter.start({
-        productName: "media-dupes",
-        companyName: "yafp",
-        submitURL: "https://sentry.io/api/1757940/minidump/?sentry_key=bbaa8fa09ca84a8da6a545c04d086859", //?
-        uploadToServer: false
-});
+  productName: 'media-dupes',
+  companyName: 'yafp',
+  submitURL: 'https://sentry.io/api/1757940/minidump/?sentry_key=bbaa8fa09ca84a8da6a545c04d086859', // ?
+  uploadToServer: false
+})
 // To simulate a crash - execute: process.crash();
 
 // ----------------------------------------------------------------------------
@@ -19,19 +19,19 @@ crashReporter.start({
 // https://sentry.io/organizations/yafp/
 // https://docs.sentry.io/platforms/javascript/electron/
 //
-const Sentry = require("@sentry/electron");
+const Sentry = require('@sentry/electron')
 Sentry.init({
-    dsn: "https://4bd3f512a1e34e24ab9838b00f57d131@sentry.io/1847606"
-});
+  dsn: 'https://4bd3f512a1e34e24ab9838b00f57d131@sentry.io/1847606'
+})
 //
 // simple way to force a crash:
-//myUndefinedFunction();
+// myUndefinedFunction();
 
-Sentry.captureMessage("Init completed");
+Sentry.captureMessage('Init completed')
 
 // ----------------------------------------------------------------------------
 // Error Handling using: electron-unhandled (https://github.com/sindresorhus/electron-unhandled)
 // ----------------------------------------------------------------------------
 //
-//const unhandled = require("electron-unhandled");
-//unhandled();
+// const unhandled = require("electron-unhandled");
+// unhandled();
--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, electron, ipcMain, Menu } = require('electron')
-const shell = require("electron").shell;
+const shell = require('electron').shell
 const path = require('path')
-
+const openAboutWindow = require('about-window').default // for: about-window
 
 // ----------------------------------------------------------------------------
 // ERROR-HANDLING
 // ----------------------------------------------------------------------------
 //
-require("./app/js/crashReporting.js");
-//myUndefinedFunctionFromMain();
-
-
+require('./app/js/crashReporting.js')
+// myUndefinedFunctionFromMain();
 
 // ----------------------------------------------------------------------------
 // VARIABLES & CONSTANTS
@@ -20,16 +18,14 @@ require("./app/js/crashReporting.js");
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-const gotTheLock = app.requestSingleInstanceLock(); // for: single-instance handling
+const gotTheLock = app.requestSingleInstanceLock() // for: single-instance handling
 
 // project-urls
-let urlGitHubGeneral = "https://github.com/yafp/media-dupes";
-let urlGitHubIssues = "https://github.com/yafp/media-dupes/issues";
-let urlGitHubChangelog = "https://github.com/yafp/media-dupes/blob/master/docs/CHANGELOG.md";
-let urlGitHubFAQ = "https://github.com/yafp/media-dupes/blob/master/docs/FAQ.md";
-let urlGitHubReleases = "https://github.com/yafp/media-dupes/releases";
-
-
+const urlGitHubGeneral = 'https://github.com/yafp/media-dupes'
+const urlGitHubIssues = 'https://github.com/yafp/media-dupes/issues'
+const urlGitHubChangelog = 'https://github.com/yafp/media-dupes/blob/master/docs/CHANGELOG.md'
+const urlGitHubFAQ = 'https://github.com/yafp/media-dupes/blob/master/docs/FAQ.md'
+const urlGitHubReleases = 'https://github.com/yafp/media-dupes/releases'
 
 // ----------------------------------------------------------------------------
 // FUNCTIONS
@@ -43,11 +39,11 @@ let urlGitHubReleases = "https://github.com/yafp/media-dupes/releases";
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1200,
+    width: 800,
     minWidth: 800,
-    height: 900,
-    minHeight: 900,
-    icon: path.join(__dirname, "app/img/icon/icon.png"),
+    height: 800,
+    minHeight: 800,
+    icon: path.join(__dirname, 'app/img/icon/icon.png'),
 
     webPreferences: {
       nodeIntegration: true,
@@ -55,31 +51,23 @@ function createWindow () {
     }
   })
 
-
   // Call from renderer: Open folder with user configured services
-ipcMain.on("openUserDownloadFolder", (event) => {
-
-        if (shell.openItem(downloadTarget) === true)
-        {
-            console.log("Opened the user download folder (ipcMain)");
-        }
-        else
-        {
-            console.log("Failed to open the user download folder (ipcMain)");
-        }
-    });
-
-
+  ipcMain.on('openUserDownloadFolder', (event) => {
+    if (shell.openItem(downloadTarget) === true) {
+      console.log('Opened the user download folder (ipcMain)')
+    } else {
+      console.log('Failed to open the user download folder (ipcMain)')
+    }
+  })
 
   const downloadTarget = app.getPath('downloads')
-  global.sharedObj = {prop1: downloadTarget};
-
+  global.sharedObj = { prop1: downloadTarget }
 
   // and load the index.html of the app.
   mainWindow.loadFile('app/index.html')
 
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
@@ -90,352 +78,271 @@ ipcMain.on("openUserDownloadFolder", (event) => {
   })
 }
 
-
 /**
 * @name createMenu
 * @summary Creates the application menu
 * @description Creates the application menu
 */
-function createMenu()
-{
-    // Create a custom menu
-    var menu = Menu.buildFromTemplate([
+function createMenu () {
+  // Create a custom menu
+  var menu = Menu.buildFromTemplate([
 
     // Menu: File
     {
-        label: "File",
-        submenu: [
+      label: 'File',
+      submenu: [
         // Exit
         {
-            role: "quit",
-            label: "Exit",
-            click() {
-                app.quit();
-            },
-            accelerator: "CmdOrCtrl+Q"
+          role: 'quit',
+          label: 'Exit',
+          click () {
+            app.quit()
+          },
+          accelerator: 'CmdOrCtrl+Q'
         }
-    ]
+      ]
     },
 
     // Menu: Edit
     {
-        label: "Edit",
-        submenu: [
+      label: 'Edit',
+      submenu: [
         {
-            label: "Undo",
-            accelerator: "CmdOrCtrl+Z",
-            selector: "undo:"
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          selector: 'undo:'
         },
         {
-            label: "Redo",
-            accelerator: "Shift+CmdOrCtrl+Z",
-            selector: "redo:"
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          selector: 'redo:'
         },
         {
-            type: "separator"
+          type: 'separator'
         },
         {
-            label: "Cut",
-            accelerator: "CmdOrCtrl+X",
-            selector: "cut:"
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          selector: 'cut:'
         },
         {
-            label: "Copy",
-            accelerator: "CmdOrCtrl+C",
-            selector: "copy:"
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          selector: 'copy:'
         },
         {
-            label: "Paste",
-            accelerator: "CmdOrCtrl+V",
-            selector: "paste:"
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          selector: 'paste:'
         },
         {
-            label: "Select All",
-            accelerator: "CmdOrCtrl+A",
-            selector: "selectAll:"
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          selector: 'selectAll:'
         }
-    ]
+      ]
     },
 
     // Menu: View
     {
-        label: "View",
-        submenu: [
+      label: 'View',
+      submenu: [
         {
-            role: "reload",
-            label: "Reload",
-            click(item, mainWindow) {
-                mainWindow.reload();
-            },
-            accelerator: "CmdOrCtrl+R"
+          role: 'reload',
+          label: 'Reload',
+          click (item, mainWindow) {
+            mainWindow.reload()
+          },
+          accelerator: 'CmdOrCtrl+R'
         }
-    ]
+      ]
     },
 
     // Menu: Window
     {
-        label: "Window",
-        submenu: [
+      label: 'Window',
+      submenu: [
         {
-            role: "togglefullscreen",
-            label: "Toggle Fullscreen",
-            click(item, mainWindow) {
-                if(mainWindow.isFullScreen())
-                {
-                    mainWindow.setFullScreen(false);
-                }
-                else
-                {
-                    mainWindow.setFullScreen(true);
-                }
-
-            },
-            accelerator: "F11" // is most likely predefined on osx - results in: doesnt work on osx
+          role: 'togglefullscreen',
+          label: 'Toggle Fullscreen',
+          click (item, mainWindow) {
+            if (mainWindow.isFullScreen()) {
+              mainWindow.setFullScreen(false)
+            } else {
+              mainWindow.setFullScreen(true)
+            }
+          },
+          accelerator: 'F11' // is most likely predefined on osx - results in: doesnt work on osx
         },
         {
-            role: "hide",
-            label: "Hide",
-            click(item, mainWindow) {
-                mainWindow.hide();
-                //mainWindow.reload();
-            },
-            accelerator: "CmdOrCtrl+H",
-            enabled: true
+          role: 'hide',
+          label: 'Hide',
+          click (item, mainWindow) {
+            mainWindow.hide()
+            // mainWindow.reload();
+          },
+          accelerator: 'CmdOrCtrl+H',
+          enabled: true
         },
         {
-            role: "minimize",
-            label: "Minimize",
-            click(item, mainWindow) {
-                if(mainWindow.isMinimized())
-                {
-                    //mainWindow.restore();
-                }
-                else
-                {
-                    mainWindow.minimize();
-                }
-            },
-            accelerator: "CmdOrCtrl+M",
+          role: 'minimize',
+          label: 'Minimize',
+          click (item, mainWindow) {
+            if (mainWindow.isMinimized()) {
+              // mainWindow.restore();
+            } else {
+              mainWindow.minimize()
+            }
+          },
+          accelerator: 'CmdOrCtrl+M'
         },
         {
-            label: "Maximize",
-            click(item, mainWindow) {
-                if(mainWindow.isMaximized())
-                {
-                    mainWindow.unmaximize();
-                }
-                else
-                {
-                    mainWindow.maximize();
-                }
-            },
-            accelerator: "CmdOrCtrl+K",
+          label: 'Maximize',
+          click (item, mainWindow) {
+            if (mainWindow.isMaximized()) {
+              mainWindow.unmaximize()
+            } else {
+              mainWindow.maximize()
+            }
+          },
+          accelerator: 'CmdOrCtrl+K'
         }
-    ]
+      ]
     },
 
     // Menu: Help
     {
-        role: "help",
-        label: "Help",
-        submenu: [
+      role: 'help',
+      label: 'Help',
+      submenu: [
         // About
         {
-            role: "about",
-            label: "About",
-            click() {
-                openAboutWindow({
-                    icon_path: path.join(__dirname, "app/img/about/icon_about.png"),
-                    open_devtools: false,
-                    use_version_info: true,
-                    win_options:  // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#new-browserwindowoptions
+          role: 'about',
+          label: 'About',
+          click () {
+            openAboutWindow({
+              icon_path: path.join(__dirname, 'app/img/about/icon_about.png'),
+              open_devtools: false,
+              use_version_info: true,
+              win_options: // https://github.com/electron/electron/blob/master/docs/api/browser-window.md#new-browserwindowoptions
                     {
-                        autoHideMenuBar: true,
-                        titleBarStyle: "hidden",
-                        minimizable: false, // not implemented on linux
-                        maximizable: false, // not implemented on linux
-                        movable: false, // not implemented on linux
-                        resizable: false,
-                        alwaysOnTop: true,
-                        fullscreenable: false,
-                        skipTaskbar: false
+                      autoHideMenuBar: true,
+                      titleBarStyle: 'hidden',
+                      minimizable: false, // not implemented on linux
+                      maximizable: false, // not implemented on linux
+                      movable: false, // not implemented on linux
+                      resizable: false,
+                      alwaysOnTop: true,
+                      fullscreenable: false,
+                      skipTaskbar: false
                     }
-                });
-
-            },
+            })
+          }
         },
         // open homepage
         {
-            label: "Homepage",
-            click() {
-                shell.openExternal(urlGitHubGeneral);
-            },
-            accelerator: "F1"
+          label: 'Homepage',
+          click () {
+            shell.openExternal(urlGitHubGeneral)
+          },
+          accelerator: 'F1'
         },
         // report issue
         {
-            label: "Report issue",
-            click() {
-                shell.openExternal(urlGitHubIssues);
-            },
-            accelerator: "F2"
+          label: 'Report issue',
+          click () {
+            shell.openExternal(urlGitHubIssues)
+          },
+          accelerator: 'F2'
         },
         // open changelog
         {
-            label: "Changelog",
-            click() {
-                shell.openExternal(urlGitHubChangelog);
-            },
-            accelerator: "F3"
-        },
-        // open FAQ
-        {
-            label: "FAQ",
-            click() {
-                shell.openExternal(urlGitHubFAQ);
-            },
-            accelerator: "F4"
+          label: 'Changelog',
+          click () {
+            shell.openExternal(urlGitHubChangelog)
+          },
+          accelerator: 'F3'
         },
         // open Releases
         {
-            label: "Releases",
-            click() {
-                shell.openExternal(urlGitHubReleases);
-            },
-            accelerator: "F5"
+          label: 'Releases',
+          click () {
+            shell.openExternal(urlGitHubReleases)
+          },
+          accelerator: 'F4'
         },
         {
-            type: "separator"
+          type: 'separator'
         },
         // Update
         {
-            label: "Search updates",
-            click(item, mainWindow) {
-                mainWindow.webContents.send("startSearchUpdates");
-            },
-            enabled: true,
-            accelerator: "F9"
+          label: 'Search updates',
+          click (item, mainWindow) {
+            mainWindow.webContents.send('startSearchUpdates')
+          },
+          enabled: true,
+          accelerator: 'F9'
         },
         {
-            type: "separator"
+          type: 'separator'
         },
 
-        // SubMenu Console
+        // Console
         {
-            label: "Console",
-            submenu: [
-            // Console
-            {
-                id: "HelpConsole",
-                label: "Console",
-                click(item, mainWindow) {
-                    mainWindow.webContents.toggleDevTools();
-                },
-                enabled: true,
-                accelerator: "F12"
-            },
-            ]
-        },
-        {
-            type: "separator"
-        },
-        // SubMenu of help
-        {
-            label: "Maintenance",
-            submenu: [
-            // Clear cache in userData
-            {
-                id: "ClearCache",
-                label: "Clear cache",
-                click(item, mainWindow) {
-                    var chromeCacheDir = path.join(app.getPath("userData"), "Cache");
-                    if(fs.existsSync(chromeCacheDir))
-                    {
-                        var files = fs.readdirSync(chromeCacheDir);
-                        for(var i=0; i<files.length; i++)
-                        {
-                            var filename = path.join(chromeCacheDir, files[i]);
-                            if(fs.existsSync(filename))
-                            {
-                                try {
-                                    fs.unlinkSync(filename);
-                                }
-                                catch(e) {
-                                    console.log(e);
-                                }
-                            }
-                        }
-                    }
-
-                    mainWindow.reload();
-                },
-                enabled: true
-            }
-            ]
+          id: 'HelpConsole',
+          label: 'Console',
+          click (item, mainWindow) {
+            mainWindow.webContents.toggleDevTools()
+          },
+          enabled: true,
+          accelerator: 'F12'
         }
-        ]
+      ]
     }
-    ]);
+  ])
 
-    // use the menu
-    Menu.setApplicationMenu(menu);
+  // use the menu
+  Menu.setApplicationMenu(menu)
 }
-
-
-
 
 /**
 * @name forceSingleAppInstance
 * @summary Takes care that there is only 1 instance of this app running
 * @description Takes care that there is only 1 instance of this app running
 */
-function forceSingleAppInstance()
-{
-    if (!gotTheLock)
-    {
-        //writeLog("error", "forceSingleAppInstance ::: There is already another instance of tttth");
-        app.quit(); // quit the second instance
-    }
-    else
-    {
-        app.on("second-instance", (event, commandLine, workingDirectory) =>
+function forceSingleAppInstance () {
+  if (!gotTheLock) {
+    // writeLog("error", "forceSingleAppInstance ::: There is already another instance of tttth");
+    app.quit() // quit the second instance
+  } else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+      // Someone tried to run a second instance, we should focus our first instance window.
+      if (mainWindow) {
+        if (mainWindow === null) // #134
         {
-            // Someone tried to run a second instance, we should focus our first instance window.
-            if (mainWindow)
-            {
-                if (mainWindow === null) //#134
-                {
-                    // do nothing - there is no mainwindow - most likely we are on macOS
-                }
-                else // mainWindow exists
-                {
-                    if (mainWindow.isMinimized())
-                    {
-                        mainWindow.restore();
-                    }
-                    mainWindow.focus();
-                }
-            }
-        });
-    }
+          // do nothing - there is no mainwindow - most likely we are on macOS
+        } else // mainWindow exists
+        {
+          if (mainWindow.isMinimized()) {
+            mainWindow.restore()
+          }
+          mainWindow.focus()
+        }
+      }
+    })
+  }
 }
-
-
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 //
-//app.on('ready', createWindow)
-app.on("ready", function ()
-{
-    forceSingleAppInstance(); // check for single instance
-    createWindow(); // create the application UI
-    createMenu(); // create the application menu
-});
-
-
-
+// app.on('ready', createWindow)
+app.on('ready', function () {
+  forceSingleAppInstance() // check for single instance
+  createWindow() // create the application UI
+  createMenu() // create the application menu
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
