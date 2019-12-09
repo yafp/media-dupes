a263ec86d7e7e3250051785f52480e085c93d575
fidel <fidel@yafp.de>
2019-12-09 18:44:21 +0100

    20191209 - Minor changes

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -12,7 +12,7 @@ var arrayUserUrls = []
 /**
 * @name showNoty
 * @summary Shows a noty notification
-* @description Creates a notification using the noty framework
+* @description Creates an in-app notification using the noty framework
 * @param type - Options: alert, success, warning, error, info/information
 * @param message - notification text
 * @param timeout - Timevalue, defines how long the message should be displayed. Use 0 for no-timeout
@@ -28,6 +28,13 @@ function showNoty (type, message, timeout = 3000) {
   }).show()
 }
 
+/**
+* @name showNotifcation
+* @summary Shows a desktop notification
+* @description Shows a desktop notification
+* @param title - The title of the desktop notification
+* @param message - The notification message text
+*/
 function showNotifcation (title = 'media-dupes', message) {
   const myNotification = new Notification(title, {
     body: message
@@ -39,58 +46,65 @@ function showNotifcation (title = 'media-dupes', message) {
 }
 
 /**
-* @name openUserDownloadFolder
-* @summary Triggers code in main.js to open the download folder of the user
-* @description Triggers code in main.js to open the download folder of the user
+* @name checkForDeps
+* @summary Checks for missing dependencies
+* @description Checks on startup for missing dependencies
 */
-function openUserDownloadFolder () {
-  const { ipcRenderer } = require('electron')
-  ipcRenderer.send('openUserDownloadFolder')
+function checkForDeps()
+{
+  console.log("checkForDeps ::: Starting")
+
+  var countErrors = 0;
+
+  // ffmpeg
+  console.log("checkForDeps ::: Searching ffmpeg ...")
+  var ffmpeg = require('ffmpeg-static-electron');
+  if (ffmpeg === "")
+  {
+    console.error("checkForDeps ::: Unable to find ffmpeg")
+    $("#buttonStartAudio").hide();
+    showNoty("error", "Unable to find dependency <b>ffmpeg</b>. Extracting audio is now disabled, sorry", 0)
+    countErrors = countErrors + 1;
+  }
+  else
+  {
+    console.log("checkForDeps ::: Found ffmpeg in: _" + ffmpeg.path + "_.");
+  }
+
+  console.log("checkForDeps ::: Finished - found overall _" + countErrors + "_ problems.");
 }
 
+
+
 /**
-* @name resetUI
+* @name uiReset
 * @summary Resets the UI back to default
 * @description Resets the UI back to default
 */
-function resetUI () {
-  // a = []
-  // clean_a = []
-  arrayUserUrls = []
+function uiReset () {
 
   // empty input file
   $('#inputNewUrl').val('')
 
-  // disable add button
-  // $('#buttonAddUrl').prop('disabled', true)
-
-  // empty textarea
-  $('#textareaTodoList').val('')
-
   // disable start button
-  $('#buttonStartVideo').prop('disabled', true)
-  $('#buttonStartAudio').prop('disabled', true)
+  startButtonsDisable()
+
+    // empty todo-list textarea
+  toDoListReset()
 
   // empty log textarea
-  $('#textareaLogOutput').val('')
+  uiLogReset()
 
-  console.log('resetUI ::: finished resetting the UI')
+  console.log('uiReset ::: finished resetting the UI')
 }
 
-function validateUserInput () {
-  // get content of input
-  var newUrl = $('#inputNewUrl').val()
-
-  console.log('Validating user input: ' + newUrl)
-
-  isUrlValid = validURL(newUrl)
-  if (isUrlValid) {
-    console.log('VALID')
-  } else {
-    console.error('invalid')
-  }
-}
-
+/**
+* @name validURL
+* @summary checks if a given string is a valid url
+* @description checks if a given string is a valid url
+* @param str - Given url
+* @return boolean
+*/
 function validURL (str) {
   var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
@@ -113,7 +127,7 @@ function addURL () {
   if (newUrl !== '') {
     isUrlValid = validURL(newUrl)
     if (isUrlValid) {
-      console.log('Adding new url: ' + newUrl)
+      console.log('addURL ::: Adding new url: ' + newUrl)
 
       // append to array
       arrayUserUrls.push(newUrl)
@@ -124,12 +138,15 @@ function addURL () {
       // reset input
       $('#inputNewUrl').val('')
 
-      // disable button
-      // $('#buttonAddUrl').prop('disabled', true)
-    } else {
-      console.error('invalid')
-      showNoty('error', 'Please insert a valid url')
+    } 
+    else {
+      console.error('addURL ::: Detected invalid url: ' + newUrl)
+      showNoty('error', 'Please insert a valid url (invalid)')
     }
+  }
+  else
+  {
+    showNoty('error', 'Please insert a valid url (empty)')
   }
 }
 
@@ -141,21 +158,180 @@ function addURL () {
 function updateToDoList () {
   // remove duplicate entries in array
   arrayUserUrls = $.unique(arrayUserUrls)
-  // a = clean_a
 
   // write array content to textarea
   var textarea = document.getElementById('textareaTodoList')
   textarea.value = arrayUserUrls.join('\n')
 
-  // if array size > 0
-  // enable start button
+  // if array size > 0 -> enable start button
   var arrayLength = arrayUserUrls.length
   if (arrayLength > 0) {
-    $('#buttonStartVideo').prop('disabled', false)
-    $('#buttonStartAudio').prop('disabled', false)
+    startButtonsEnable()
   }
 
+  // TODO:
+  // change background of single lines odd/even
+
   console.log('updateToDoList ::: Added new url to todo-list')
+}
+
+/**
+* @name toDoListReset
+* @summary Resets the todo-list textarea
+* @description Resets the todo-list textarea
+*/
+function toDoListReset () {
+
+  // reset the array
+  arrayUserUrls = []
+
+  // reset todo-list
+  textareaTodoList.value = ''
+
+  console.log('toDoListReset ::: Did reset the todolist-textarea')
+}
+
+
+/**
+* @name startButtonsEnable
+* @summary Enabled the 2 start buttons
+* @description Is executed when the todo-list contains at least 1 item
+*/
+function startButtonsEnable()
+{
+  // disable start button
+  $('#buttonStartVideo').prop('disabled', false)
+  $('#buttonStartAudio').prop('disabled', false)
+
+  console.log('startButtonsEnable ::: Did disable both start buttons')
+}
+
+/**
+* @name startButtonsDisable
+* @summary Disables the 2 start buttons
+* @description Is executed when a download task is started by the user
+*/
+function startButtonsDisable()
+{
+  // disable start button
+  $('#buttonStartVideo').prop('disabled', true)
+  $('#buttonStartAudio').prop('disabled', true)
+
+  console.log('startButtonsDisable ::: Did disable both start buttons')
+}
+
+/**
+* @name uiLogReset
+* @summary Resetts the log textarea
+* @description Resetts the log textarea
+*/
+function uiLogReset () {
+  textareaLogOutput.value = ''
+  console.log('uiLogReset ::: Did reset the log-textarea')
+}
+
+/**
+* @name uiLogAppend
+* @summary Appends text to the log textarea
+* @description Appends text to the log textarea
+*/
+function uiLogAppend (newLine) {
+  $('#textareaLogOutput').val(function (i, text) {
+    return text + newLine + '\n'
+  })
+}
+
+/**
+* @name downloadContent
+* @summary Does the actual download
+* @description Does the actual download
+*/
+function downloadContent(mode)
+{
+  console.log('downloadContent ::: Start with mode set to: ' + mode + '.')
+
+  // example urls
+  //
+  // url = "http://www.youtube.com/watch?v=90AiXO1pAiA";
+  // url = "https://vimeo.com/120828152"
+  // url = "https://soundcloud.com/jakarta-records/suff-daddy-feat-ill-camille-jlamotta-magic-taken-from-seasons-in-jakarta"
+
+  // Prepare UI
+  //
+  uiLogReset() // start new job - clean up loag before
+  startButtonsDisable() // disable the start buttons
+
+  // require some stuff
+  const youtubedl = require('youtube-dl')
+  const { remote } = require('electron')
+  const path = require('path')
+
+
+  var targetPath = remote.getGlobal('sharedObj').prop1
+  var youtubeDlParameter = ""
+
+  // Define the youtube-dl parameters depending on the mode (audio vs video)
+  switch (mode) 
+  {
+    case "audio":
+      //youtubeDlParameter = ['-f', 'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--audio-quality', '0', '-o', path.join(targetPath, 'media-dupes', '%(title)s-%(id)s.%(ext)s')]
+      //
+      // --prefer-ffmpeg
+      //youtubeDlParameter = ['-f', 'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--audio-quality', '0', '-o', path.join(targetPath, 'media-dupes', '%(title)s-%(id)s.%(ext)s'), '--prefer-ffmpeg']
+      //
+      // --prefer-avconv
+      //youtubeDlParameter = ['-f', 'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--audio-quality', '0', '-o', path.join(targetPath, 'media-dupes', '%(title)s-%(id)s.%(ext)s'), '--prefer-avconv']
+
+      var ffmpeg = require('ffmpeg-static-electron');
+      console.log(ffmpeg.path);
+
+      youtubeDlParameter = ['-f', 'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--audio-quality', '0', '-o', path.join(targetPath, 'media-dupes', '%(title)s-%(id)s.%(ext)s'), '--prefer-ffmpeg', '--ffmpeg-location', ffmpeg.path]
+      break;
+
+    case "video":
+      youtubeDlParameter = ['-f', 'best', '-o', path.join(targetPath, 'media-dupes', '%(title)s-%(id)s.%(ext)s')]
+      break;
+
+    default:
+      showNoty("error", "Unexpected download case. Please report this issue")
+      break;
+  }
+
+  // for each item of the array
+  //
+  var arrayLength = arrayUserUrls.length
+  for (var i = 0; i < arrayLength; i++) {
+    var url = arrayUserUrls[i]
+
+    console.log('downloadContent ::: Processing URL: ' + url)
+    uiLogAppend('Processing: ' + url)
+
+    const video = youtubedl.exec(url, youtubeDlParameter, {}, function (err, output) {
+      if (err)
+      {
+        throw err
+        showNoty("error", "Downloading <b>" + url + "</b> failed with error: " + err)
+      }
+
+      // show progress
+      console.log(output.join('\n'))
+      uiLogAppend(output.join('\n'))
+
+      // finish
+      console.log('downloadContent ::: Finished downloading ' + url)
+      uiLogAppend('Finished downloading: ' + url)
+      showNoty('success', 'Finished downloading <b>' + url + '</b>.')
+
+      // Final notification
+      if (i === arrayLength) {
+        showNotifcation('media-dupes', 'Finished downloading ' + i + ' url(s).')
+        toDoListReset()
+      }
+    })
+  }
+
+  console.log('downloadContent ::: All download processes are now at least started')
+
 }
 
 /**
@@ -185,134 +361,6 @@ function showSupportedExtractors () {
 }
 
 /**
-* @name resetToDoList
-* @summary Resets the todo-list textarea
-* @description Resets the todo-list textarea
-*/
-function resetToDoList () {
-  textareaTodoList.value = ''
-  console.log('resetToDoList ::: Did reset the todolist textarea')
-}
-
-/**
-* @name resetUiLog
-* @summary Resetts the log textarea
-* @description Resetts the log textarea
-*/
-function resetUiLog () {
-  textareaLogOutput.value = ''
-  console.log('resetUiLog ::: Did reset the log textarea')
-}
-
-/**
-* @name uiLogAppend
-* @summary Appends text to the log textarea
-* @description Appends text to the log textarea
-*/
-function uiLogAppend (newLine) {
-  $('#textareaLogOutput').val(function (i, text) {
-    return text + newLine + '\n'
-  })
-}
-
-/**
-* @name downloadVideo
-* @summary Download vido
-* @description Download vido
-*/
-function downloadVideo () {
-  console.log('downloadVideo ::: Start')
-
-  resetUiLog()
-
-  const youtubedl = require('youtube-dl')
-  const { remote } = require('electron')
-
-  var targetPath = remote.getGlobal('sharedObj').prop1
-
-  // url = "http://www.youtube.com/watch?v=90AiXO1pAiA";
-  // url = "https://vimeo.com/120828152"
-  // url = "https://soundcloud.com/jakarta-records/suff-daddy-feat-ill-camille-jlamotta-magic-taken-from-seasons-in-jakarta"
-
-  // for each item of the array
-  var arrayLength = arrayUserUrls.length
-  for (var i = 0; i < arrayLength; i++) {
-    var url = arrayUserUrls[i]
-    console.log('downloadVideo ::: Processing URL: ' + url)
-    uiLogAppend('Processing: ' + url)
-
-    console.log('downloadVideo ::: Target is set to: ' + targetPath)
-
-    youtubedl.exec(url, ['-o', targetPath + '/%(title)s-%(id)s.%(ext)s'], {}, function (err, output) {
-      if (err) throw err
-
-      console.log(output.join('\n'))
-      uiLogAppend(output.join('\n'))
-
-      console.log('downloadVideo ::: Finished downloading ' + url)
-      uiLogAppend('Finished downloading: ' + url)
-      showNoty('success', 'Finished downloading ' + url)
-
-      // Final notification
-      if (i === arrayLength) {
-        showNotifcation('media-dupes', 'Finished downloading ' + i + ' url(s).')
-        resetToDoList()
-      }
-    })
-  }
-
-  // showNotifcation("","Finished downloading " + i + " videos.")
-
-  console.log('downloadVideo ::: End')
-}
-
-/**
-* @name downloadAudio
-* @summary Download audio
-* @description Download audio
-*/
-function downloadAudio () {
-  console.log('downloadAudio ::: Start')
-
-  resetUiLog()
-
-  const youtubedl = require('youtube-dl')
-  const { remote } = require('electron')
-
-  var targetPath = remote.getGlobal('sharedObj').prop1
-
-  // for each item of the array
-  var arrayLength = arrayUserUrls.length
-  for (var i = 0; i < arrayLength; i++) {
-    var url = arrayUserUrls[i]
-    console.log('downloadAudio ::: Processing URL: ' + url)
-    uiLogAppend('Processing: ' + url)
-
-    console.log('downloadAudio ::: Target is set to: ' + targetPath)
-
-    // youtubedl.exec(url, ['-x', '--audio-format', 'mp3'], {}, function(err, output)
-    // youtubedl.exec(url, ['-f',  'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--audio-quality', '0'], {}, function(err, output)
-    const video = youtubedl.exec(url, ['-f', 'bestaudio', '--extract-audio', '--audio-format', 'mp3', '--audio-quality', '0', '-o', targetPath + '/%(title)s-%(id)s.%(ext)s'], {}, function (err, output) {
-      if (err) throw err
-      console.log(output.join('\n'))
-      uiLogAppend(output.join('\n'))
-
-      console.log('downloadAudio ::: finished downloading ' + url)
-      uiLogAppend('Finished downloading: ' + url)
-      showNoty('success', 'Finished downloading ' + url)
-
-      // Final notification
-      if (i === arrayLength) {
-        showNotifcation('media-dupes', 'Finished downloading ' + i + ' url(s).')
-        resetToDoList()
-      }
-    })
-  }
-
-  console.log('downloadAudio ::: End')
-}
-
-/**
 * @name searchUpdate
 * @summary Checks if there is a new release available
 * @description Compares the local app version number with the tag of the latest github release. Displays a notification in the settings window if an update is available.
@@ -330,7 +378,7 @@ function searchUpdate (silent = true) {
   var gitHubPath = 'yafp/media-dupes' // user/repo
   var url = 'https://api.github.com/repos/' + gitHubPath + '/tags'
 
-  // writeLog("info", "searchUpdate ::: Start checking _" + url + "_ for available releases");
+  console.log("searchUpdate ::: Start checking _" + url + "_ for available releases");
 
   var updateStatus = $.get(url, function (data) {
     timeout:3000 // in milliseconds
@@ -350,12 +398,12 @@ function searchUpdate (silent = true) {
     localAppVersion = require('electron').remote.app.getVersion()
     // localAppVersion = "1.0.0"; // to simulate
 
-    // writeLog("info", "searchUpdate ::: Local version: " + localAppVersion);
-    // writeLog("info", "searchUpdate ::: Latest public version: " + remoteAppVersionLatest);
+    console.log("searchUpdate ::: Local version: " + localAppVersion);
+    console.log("searchUpdate ::: Latest public version: " + remoteAppVersionLatest);
 
     if (localAppVersion < remoteAppVersionLatest) // Update available
     {
-      // writeLog("warn", "searchUpdate ::: Found update, notify user");
+      console.log("searchUpdate ::: Found update, notify user");
 
       // using a confirm dialog - since #150
       const Noty = require('noty')
@@ -384,7 +432,7 @@ function searchUpdate (silent = true) {
       n.show()
     } else // No update available
     {
-      // writeLog("info", "searchUpdate ::: No newer version found.");
+      console.log("searchUpdate ::: No newer version found.");
 
       if (silent === false) // when executed manually via menu -> user should see result of this search
       {
@@ -392,19 +440,19 @@ function searchUpdate (silent = true) {
       }
     }
 
-    // writeLog("info", "searchUpdate ::: Successfully checked " + url + " for available releases");
+    console.log("searchUpdate ::: Successfully checked " + url + " for available releases");
   })
     .done(function () {
-      // writeLog("info", "searchUpdate ::: Successfully checked " + url + " for available releases");
+      console.log("searchUpdate ::: Successfully checked " + url + " for available releases");
     })
 
     .fail(function () {
-      // writeLog("error", "searchUpdate ::: Checking " + url + " for available releases failed.");
+      console.log("searchUpdate ::: Checking " + url + " for available releases failed.");
       showNoty('error', 'Checking <b>' + url + '</b> for available releases failed. Please troubleshoot your network connection.')
     })
 
     .always(function () {
-      // writeLog("info", "searchUpdate ::: Finished checking " + url + " for available releases");
+      console.log("searchUpdate ::: Finished checking " + url + " for available releases");
     })
 }
 
@@ -415,7 +463,7 @@ function searchUpdate (silent = true) {
 */
 function openReleasesOverview () {
   var url = 'https://github.com/yafp/media-dupes/releases'
-  // writeLog("info", "openReleasesOverview ::: Opening _" + url + "_ to show available releases");
+  console.log("openReleasesOverview ::: Opening _" + url + "_ to show available releases");
   openURL(url)
 }
 
@@ -427,8 +475,18 @@ function openReleasesOverview () {
 */
 function openURL (url) {
   const { shell } = require('electron')
-  // writeLog("info", "openURL ::: Trying to open the url: " + url);
+  console.log("openURL ::: Trying to open the url: " + url);
   shell.openExternal(url)
+}
+
+/**
+* @name openUserDownloadFolder
+* @summary Triggers code in main.js to open the download folder of the user
+* @description Triggers code in main.js to open the download folder of the user
+*/
+function openUserDownloadFolder () {
+  const { ipcRenderer } = require('electron')
+  ipcRenderer.send('openUserDownloadFolder')
 }
 
 // Call from main.js ::: startSearchUpdates
