0a5860aad0ed3aac6ff1bfb25d531e4779f9f331
fidel <fidel@yafp.de>
2019-12-10 20:00:02 +0100

    20191210 - Adopting script to js standard

--- app/js/crashReporting.js
+++ app/js/crashReporting.js
@@ -5,10 +5,10 @@
 //
 const { crashReporter } = require('electron')
 crashReporter.start({
-  productName: 'media-dupes',
-  companyName: 'yafp',
-  submitURL: 'https://sentry.io/api/1757940/minidump/?sentry_key=bbaa8fa09ca84a8da6a545c04d086859', // ?
-  uploadToServer: false
+    productName: 'media-dupes',
+    companyName: 'yafp',
+    submitURL: 'https://sentry.io/api/1757940/minidump/?sentry_key=bbaa8fa09ca84a8da6a545c04d086859', // ?
+    uploadToServer: false
 })
 // To simulate a crash - execute: process.crash();
 
@@ -21,7 +21,7 @@ crashReporter.start({
 //
 const Sentry = require('@sentry/electron')
 Sentry.init({
-  dsn: 'https://4bd3f512a1e34e24ab9838b00f57d131@sentry.io/1847606'
+    dsn: 'https://4bd3f512a1e34e24ab9838b00f57d131@sentry.io/1847606'
 })
 //
 // simple way to force a crash:
