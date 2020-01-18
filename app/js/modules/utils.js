d4fdd1799d3c93da257905cf123269ea285bda2c
fidel <fidel@yafp.de>
2020-01-18 19:48:21 +0100

    20200118 - Moving more functions to utils

--- app/js/modules/utils.js
+++ app/js/modules/utils.js
@@ -7,6 +7,72 @@
 'use strict'
 
 /**
+* @name writeConsoleMsg
+* @summary Writes console output for the renderer process
+* @description Writes console output for the renderer process
+* @param type - String which defines the log type
+* @param message - String which defines the log message
+*/
+function writeConsoleMsg (type, message) {
+    const prefix = '[ Renderer ] '
+    const log = require('electron-log')
+    // electron-log can: error, warn, info, verbose, debug, silly
+    switch (type) {
+    case 'info':
+        log.info(prefix + message)
+        break
+
+    case 'warn':
+        log.warn(prefix + message)
+        break
+
+    case 'error':
+        log.error(prefix + message)
+        break
+
+    default:
+        log.silly(prefix + message)
+    }
+}
+
+/**
+* @name showNoty
+* @summary Shows a noty notification
+* @description Creates an in-app notification using the noty framework
+* @param type - Options: alert, success, warning, error, info/information
+* @param message - notification text
+* @param timeout - Timevalue, defines how long the message should be displayed. Use 0 for no-timeout
+*/
+function showNoty (type, message, timeout = 3000) {
+    const Noty = require('noty')
+    new Noty({
+        type: type,
+        timeout: timeout,
+        theme: 'bootstrap-v4',
+        layout: 'bottom',
+        text: message
+    }).show()
+}
+
+/**
+* @name showNotification
+* @summary Shows a desktop notification
+* @description Shows a desktop notification
+* @param title - The title of the desktop notification
+* @param message - The notification message text
+*/
+function showNotification (title = 'media-dupes', message) {
+    const myNotification = new Notification(title, {
+        body: message,
+        icon: 'img/notification/icon.png'
+    })
+
+    myNotification.onclick = () => {
+        writeConsoleMsg('info', 'showNotification ::: Notification clicked')
+    }
+}
+
+/**
 * @name openURL
 * @summary Opens an url in browser
 * @description Opens a given url in default browser. This is pretty slow, but got no better solution so far.
@@ -14,7 +80,7 @@
 */
 function openURL (url) {
     const { shell } = require('electron')
-    // doLogToConsole('info', 'openURL ::: Trying to open the url: _' + url + '_.')
+    writeConsoleMsg('info', 'openURL ::: Trying to open the url: _' + url + '_.')
     shell.openExternal(url)
 }
 
@@ -97,6 +163,9 @@ function pathExists (path) {
     }
 }
 
+module.exports.writeConsoleMsg = writeConsoleMsg
+module.exports.showNoty = showNoty
+module.exports.showNotification = showNotification
 module.exports.openURL = openURL
 module.exports.validURL = validURL
 module.exports.formatBytes = formatBytes
